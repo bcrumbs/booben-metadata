0ad56561fd8f1e345f0a67f9e2b8a2bf42f61ee3
Dmitriy Bizyaev <dmitriy.bizyaev@gmail.com>
2017-07-12 19:25:30 +0300

    fixed lint errors

--- bin/cli.js
+++ bin/cli.js
@@ -6,11 +6,11 @@
 
 'use strict';
 
-const co = require('co'),
-  fs = require('mz/fs'),
-  path = require('path'),
-  minimist = require('minimist'),
-  gatherMetadata = require('../lib/gatherMetadata');
+const co = require('co');
+const fs = require('mz/fs');
+const path = require('path');
+const minimist = require('minimist');
+const gatherMetadata = require('../lib/gatherMetadata');
 
 co(function* () {
   const argv = minimist(process.argv.slice(2));
@@ -18,13 +18,15 @@ co(function* () {
   
   const libraryPath = path.resolve(process.cwd(), argv._[0]);
   
-  if (!(yield fs.exists(libraryPath)))
+  if (!(yield fs.exists(libraryPath))) {
     throw new Error(`${libraryPath} does not exist`);
+  }
   
   const stats = yield fs.stat(libraryPath);
   
-  if (!stats.isDirectory())
+  if (!stats.isDirectory()) {
     throw new Error(`${libraryPath} is not a directory`);
+  }
   
   yield gatherMetadata(libraryPath);
   console.log(`Metadata is OK in ${libraryPath}`);
